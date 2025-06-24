f5569404c38bfd8b35253694a757b411faca4d42
theaditya13 <theadityapersonal@gmail.com>
2025-06-24 10:19:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert("Enter Click OK to continue.");
+// document.addEventListener('DOMContentLoaded', function() {
+//     const navSvg = document.getElementById('nav-svg');
+//     const navUl = document.querySelector('.header ul');
+//     if (navSvg && navUl) {
+//         navSvg.addEventListener('click', function() {
+//             if (navUl.style.display === 'flex') {
+//                 navUl.style.display = 'none';
+//             } else {
+//                 navUl.style.display = 'flex';
+//                 navUl.style.flexDirection = 'column';
+//                 navUl.style.alignItems = 'center';
+//             }
+//         });
+//     }
+// });
